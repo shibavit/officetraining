2c2e93e1fcc885918b522c9dfbd6c45ff64e5839
shiba salvi <ssalvi@vidushigoc.local>
2018-07-12 19:15:18 +0530

    add to cart function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { ItemsService } from '../items.service';
 })
 export class HomeComponent implements OnInit {
 
+  x: any;
   constructor(private item:ItemsService) { }
 
   ngOnInit() {
@@ -15,8 +16,18 @@ export class HomeComponent implements OnInit {
 
   itemget = this.item.items;
 addition = 0;
-add(x) {
+add(i) {
+  
+  if(i.text == "Add to cart") {
   //alert( (this.item[1].price * this.item[1].qty) + (this.item[2].price * this.item[2].qty) + (this.item[3].price * this.item[3].qty));
-   this.addition = this.addition + x.price * x.qty;
+   this.addition = this.addition + i.price * i.qty;
+   i.text = "Remove from cart";  
+  }
+  else {
+   this.addition = this.addition - i.price * i.qty;
+   i.text = "Add to cart";  
+  
+  }
+ 
 }
 }
--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -5,22 +5,26 @@ import { Injectable } from '@angular/core';
 })
 export class ItemsService {
 
+  text: string;
   constructor() { }
 
   items = [ {
     id:"item1",
     price:20,
-    qty:1
+    qty:1,
+    text : "Add to cart"
 },
 {
   id:"item2",
   price:30,
-  qty:2
+  qty:2,
+  text : "Add to cart"
 },
 {
   id:"item3",
   price:10,
-  qty:3
+  qty:3,
+  text : "Add to cart"
 }
 ];
 }
